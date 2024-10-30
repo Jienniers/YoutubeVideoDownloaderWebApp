5dde0fa67751f08d44f21dc05e26cf9377eedaca
Jeniear <135406459+Jienniers@users.noreply.github.com>
2024-10-30 23:35:42 +0500

    Minor Changes to Backend (app.py)

--- app.py
+++ app.py
@@ -3,28 +3,20 @@ from pytubefix import YouTube
 import os
 import threading
 import time
+from pytubefix.cli import on_progress
+import shutil
 
 app = Flask(__name__)
 
-
-def get_video_resolutions(url):
-    try:
-        yt = YouTube(url, use_po_token=True)
-        highest_resolution = yt.streams.get_highest_resolution()
-        return [f"{highest_resolution.resolution} (Highest)"]
-    except Exception as e:
-        print(f"Error: {e}")
-        return []
-
-
 def download_video(url):
     try:
-        yt = YouTube(url, use_po_token=True)
-        highest_resolution = yt.streams.filter(adaptive=True).filter(mime_type='video/mp4').first()
-        if highest_resolution:
-            highest_resolution.download()
-            print(f"Downloading {highest_resolution.resolution} resolution...")
-            return highest_resolution.default_filename
+        yt = YouTube(url, use_po_token=True, on_progress_callback = on_progress)
+        resoltuion = yt.streams.filter(res="1080p").first()
+        if resoltuion:
+            resoltuion.download("Videos/")
+            print(f"Downloading: {yt.title}")
+            print(f"Downloading {resoltuion.resolution} resolution...")
+            return resoltuion.default_filename
         else:
             print("No streams available for the video.")
             return None
@@ -32,15 +24,9 @@ def download_video(url):
         print(f"Error: {e}")
         return None
 
-
-def delete_file_after_delay(file_path, sent_flag, delay_seconds=5):
+def delete_file_after_delay(delay_seconds=5):
     time.sleep(delay_seconds)
-    try:
-        if sent_flag[0]:
-            os.remove(file_path)
-            print(f"File deleted: {file_path}")
-    except Exception as e:
-        print(f"Error deleting file: {e}")
+    shutil.rmtree("Videos")
 
 stored_url = ""
 @app.route('/', methods=['GET', 'POST'])
@@ -49,8 +35,10 @@ def home():
     thumbnail = ""
     title = ""
     visibility = "hidden"
+
     if request.method == 'POST':
         url_text = request.form['search_url']
+
         if 'search' in request.form:
             if 'search_url' in request.form and 'https' in url_text.lower():
                 stored_url = url_text
@@ -63,22 +51,22 @@ def home():
         if "download_button_mine" in request.form:
             print(stored_url)
             status = "status: Downloading"
-            desired_resolution = request.form.get('dropdown', '720')
-            video_path = download_video(stored_url)
-            print(video_path)
-            print(desired_resolution)
+            video_path = "Videos/" + download_video(stored_url)
+
             if video_path:
-                sent_flag = [False]
                 @after_this_request
                 def remove_file(response):
-                    threading.Thread(target=delete_file_after_delay, args=(video_path, sent_flag)).start()
+                    threading.Thread(target=delete_file_after_delay).start()
                     return response
-                sent_flag[0] = True
+                
+
                 return send_file(
                     video_path,
                     as_attachment=True,
                     download_name=os.path.basename(video_path)
                 )
+            else:
+                return "File download failed", 404
             
     return render_template(
         'index.html', 
